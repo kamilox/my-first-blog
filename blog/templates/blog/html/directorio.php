b9a558de5ffdb0c1d9a0b8890797f60a7854e608
kamilox <contreras.camilo@gmail.com>
2016-05-02 16:51:31 -0430

    cambie el html para la página.

---
+++ blog/templates/blog/html/directorio.php
@@ -0,0 +1,10 @@
+<div class="row directorio">
+	<div class="col-lg-12 col-md-12 col-xs-12" >
+		
+	</div>
+</div>
+<style type="text/css">
+	.directorio{
+		height: 250px;
+	}
+</style>
---
+++ blog/templates/blog/post_list.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--responsive-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<!-- contenido para seo -->
+		<title>Clinica</title>
+		<meta name="title" content="Clinicas, Medicos, Salud, Laboratorios">
+		<meta name="Keywords" content="Clinica, Medicina, Medico, Laboratorio, Salud, Saludable, Enfermedad, Enfermo, Virus">
+		<meta name="description" content="Direcciones, ubicación y teléfonos de , Clinica,  Municipio , Estado,  Cuidados post operatorios,  Vida Saludable, Examenes">
+
+		<meta name="author" content="Salud">
+		<meta name="robots" content="index,follow">
+		<meta name="googlebot" content="index,follow">
+		<meta name="country" content="Venezuela, Estado">
+		<!-- Links para seo -->
+		<!--<link rel="prev" href="base.html" title="base - document base uri" />-->
+		<link rel="next" href="quienes.php" title="Quienes somos" />
+		<link rel="start" href="index.php/" title="Clinica" />
+		<link rel="copyright" href="#" title="Camilo Contreras" />
+		<link rev="made" href="mailto:contreras.camilo@gmail.com" title="freelancer" />
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="images/images/favicon.ico">
+		<!-- Bootstrap -->
+		<!--<link href="bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		<!-- Latest compiled and minified JavaScript -->
+		<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		
+		<!-- CSS Generales-->
+	    <link rel="stylesheet"  type="text/css" href="css/standart.css">
+	    <!-- css especificos-->
+	    <link rel="stylesheet"  type="text/css" href="css/index.css">
+		<!--menu responsive
+		<link rel="stylesheet" href="SlickNav/demo/style.css">
+		<link rel="stylesheet" href="SlickNav/dist/slicknav.css">
+		<script src="SlickNav/dist/jquery.slicknav.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>-->
+		<!--Fuentes -->
+		<link href='http://fonts.googleapis.com/css?family=Dosis:300,400|Lato:300,400' rel='stylesheet' type='text/css'>
+    
+	</head>
+	<!-- Encabezado -->
+	<header>
+	<div class="row head">
+	<div class="col-lg-2 col-md-2 col-xs-2 logo_">
+		<img src="images/header/medical_1.png" class="logo">
+	</div>
+		<h1 class="nombre">Clinica Lorem Ipsum</h1>
+		<div class="col-lg-10 col-md-10 col-xs-10 redes">
+		<img src="images/header/facebook.png" class="red1">
+		<img src="images/header/instagram.png" class="red">
+		<img src="images/header/twitter.png" class="red">
+	</div>
+	<div class="col-lg-10 col-md-10 col-xs-10 login">
+		<button type="button" class="btn btn-default"><a href="html/login.php">Entrar</a></button>
+		<button type="button" class="btn btn-default"><a href="html/resgistro.php">Registrarse</a></button>
+	</div>
+	<div class="col-lg-10 col-md-10 col-xs-10 menu"> <!-- menu -->
+		<div class="btn-group" role="group" aria-label="...">
+		 	<button type="button" class="btn btn-default"><a href="post_list.php">Inicio</a></button>
+		  	<button type="button" class="btn btn-default"><a href="#.php">Quienes Somos</a></button>
+		  	<!-- Boton descendente -->
+		  		<div class="btn-group" role="group">
+		    		<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			     		<a>Servicios</a>
+			      		<span class="caret"></span>
+		    		</button>
+		    			<ul class="dropdown-menu">
+		      				<li><a href="#">Médicos</a></li>
+		      				<li><a href="#">Laboratorio</a></li>
+		      				<li role="separator" class="divider"></li>
+		      				<li><a href="#">Comerciales</a></li>
+		      				<li><a href="#">Gubernamentales</a></li>
+		      				<li role="separator" class="divider"></li>
+		      				<li><a href="#">Usuarios</a></li>
+
+		    			</ul>
+		  		</div>
+		  	<button type="button" class="btn btn-default"><a href="#">Directorio</a></button>
+		  	<button type="button" class="btn btn-default"><a href="#">Admisión</a></button>
+		  	<button type="button" class="btn btn-default"><a href="#">Noticias</a></button>
+		  	<button type="button" class="btn btn-default"><a href="#">Empleo</a></button>
+		  	<button type="button" class="btn btn-default contacto"><a href="#">Contactenos</a></button>
+		</div>
+	</div>
+</div><!--  cierre de menu -->
+<!-- Css -->
+<style type="text/css">
+	.nombre {
+	    font-size: 36px;
+	    margin-bottom: -10%;
+	    text-align: center;
+	    padding-right: 12%;
+	    color: #0000a3;
+	}
+	.row{
+		margin-right: 0px;
+    	margin-left: 0px;
+	}
+	.head{
+		/*height: 50px;*/
+	}
+	.head, .dropdown-menu, ul, li{
+		background: #fff;
+	}
+	.logo_{
+		/*margin-top: 9%;
+	    margin-bottom: -13%;
+	    z-index: 2;*/
+	}
+	.logo{
+		width: 100%;
+	}
+	.login{
+		text-align: right;
+		margin: 1% 0% 0% 0%;
+	}
+	.login>button{
+		margin: 0% 1% 0% 0%;
+	}
+	.redes{
+		/*margin: 1% 0% 1% 0%;*/
+	}
+	.red, .red1{
+		float: right;
+		height: 50px;
+	}
+	.red1{
+		margin-right: 1.5%;
+	}
+	/* Menu */
+	.menu {
+    	margin-top: 1%;
+    	padding-right: 2%;
+	}
+	.btn-group{
+		float: right;
+		padding-right: 0%;
+		background: #fff;
+		opacity: 1;
+	}
+	.btn-default {
+    	text-shadow: none;
+    	box-shadow: none;
+    }
+	.btn{
+		background: transparent !important;
+		border-radius: 0px !important;
+		border:none !important;
+	}
+	 .btn:hover, .btn-default:hover{
+		background: #f0f0f0 !important;
+		opacity: .8 !important;
+		border-radius: 4px !important;
+		color: #ffffff !important;
+	}
+	
+	.caret{
+		color: #000;
+	}
+	a{
+		color: #000 !important;
+		font-size: 16px;
+	}
+	ul{
+		float: left;
+	}
+
+@media screen and (max-width: 1080px) {
+	.nombre {
+	    font-size: 36px;
+	    margin-bottom: -10%;
+	    text-align: center;
+	    padding-right: 22%;
+	}
+	.red, .red1{
+		height: 40px;
+	}
+		/* Menu */
+	.menu {
+		padding-right: 1%;
+	}
+	a{
+		font-size: 14px;
+	}
+	
+}
+@media screen and (max-width: 800px) {
+	a, p{
+		font-size: 11px;
+	}
+	h3{
+		font-size: 18px;
+	}
+}
+
+</style>
+
+		<!--<section>
+			<?php include('html/header.php') ?>
+		</section>
+	</header>-->
+	<!-- Cierra Encabezado -->
+	<body>
+			<section>
+				<div class="row">
+					<?php include('html/carousel.php') ?>
+				</div>	
+			</section>
+		<div class="container"><!-- Contenedor--> 
+			<section>
+				<div class="row">
+					<?php include('html/quienes.php') ?>
+				</div>	
+			</section>
+			<section>
+				<div class="row">
+					<?php include('html/servicios.php') ?>
+				</div>	
+			</section>
+		</div><!-- Cierra el Contenedor -->
+		<div class="container"> <!-- Contenedor-->
+			<section>
+				<div class="row">
+					<?php include('html/directorio.php') ?>
+				</div>	
+			</section>
+		</div><!-- Cierra el Contenedor -->
+	<!-- Pie de Página -->
+	<footer>
+		<div class="row">
+			<section>
+				<?php include('html/footer.php') ?>
+			</section>
+		</div>
+	</footer>
+	<!-- Cierra el Pie de Página -->
+			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+			<!-- Include all compiled plugins (below), or include individual files as needed -->
+			<script src="js/jquery-2.2.0.js"></script>
+			<!--<script src="bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>-->
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+			<!-- script responsive del menu 
+			<script src="SlickNav/dist/jquery.slicknav.js"></script>
+				<script type="text/javascript">
+					$(document).ready(function(){
+					  $('#menu').slicknav();
+					});
+				</script> -->
+		
+	</body>
+</html>
